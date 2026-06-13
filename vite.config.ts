d9db2b601f80501aa2d089f75b3321b6d7e933f5
Bastiasa <leysantqm@gmail.com>
2026-06-13 13:46:06 -0500

    Set dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: "docs"
+    outDir: "dist"
   },
   base: "/portfolio",
   plugins: [
